144dbd373981e7705036da14abd9bf00a4944af6
Aleksandr Babynin <babynin.home@gmail.com>
2026-04-03 23:27:57 +1100

    Move data from template to view

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+import json
+
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from places.models import Place
+
+
+def serialize_place(place):
+    return {
+        'title': place.title,
+        'imgs': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat,
+        },
+    }
+
+
+def serialize_geojson(places):
+    features = [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat],
+            },
+            'properties': {
+                'title': place.title,
+                'placeId': place.id,
+                'detailsUrl': f'/places/{place.id}/',
+            },
+        }
+        for place in places
+    ]
+    return {
+        'type': 'FeatureCollection',
+        'features': features,
+    }
 
 
 def show_map(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    context = {
+        'places_geojson': json.dumps(serialize_geojson(places), ensure_ascii=False),
+    }
+    return render(request, 'index.html', context)
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place.objects.prefetch_related('images'), id=place_id)
+    return JsonResponse(serialize_place(place), json_dumps_params={'ensure_ascii': False})
