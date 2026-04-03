671a01cfbc526efde0c28b02761623f64409b650
Aleksandr Babynin <babynin.home@gmail.com>
2026-04-03 23:15:30 +1100

    Setup media files serving

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from mysite.views import show_map
+
+from mysite.views import place_detail, show_map
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', show_map)
+    path('', show_map),
+    path('places/<int:place_id>/', place_detail),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
